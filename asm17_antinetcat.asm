62d5a35734e93d790c6116bfd57e00f158c1fd92
ginsama01 <ginsama2001@gmail.com>
2022-06-02 16:50:41 +0700

    asm17

--- asm17_antinetcat.asm
+++ asm17_antinetcat.asm
@@ -1,18 +1,33 @@
 O_RDONLY            equ 0
 SYS_OPEN            equ 2
+SYS_CLOSE           equ 3
 SYS_GETDENTS64      equ 0xd9
 SYS_WRITE           equ 1
+SYS_READ            equ 0
 SYS_EXIT            equ 0x3c
+SYS_KILL            equ 0x3e
+SYS_NANOSLEEP       equ 0x23
+SIGKILL             equ 9
 STDIN               equ 0
 STDOUT              equ 1
 
+
 section .data
-    filename    db  "/home/huynq/Documents", 0x0
+    filename    db  "/proc", 0x0
+    proc        db  "/proc/"
+    cmdline     db  "/cmdline"
     newline     db 0xA, 0x0
+
 section .bss
     fd          resq 1
     nread       resq 1
     dirent      resb 10000000
+    pid         resq 1
+    pidfile     resq 3
+    pidcmd      resb 2
+    pidfd       resq 1
+    num         resq 1
+    sleeptime   resq 2
 
 struc LINUX_DIRENT64
     .d_ino       resq 1
@@ -20,6 +35,11 @@ struc LINUX_DIRENT64
     .d_reclen    resw 1
     .d_type      resb 1
     .d_name      resb 32
+endstruc
+
+struc timespec 
+    .tv_sec     resq 1
+    .tv_nsec    resq 1
 endstruc
 
 section .text
@@ -31,6 +51,7 @@ _start:
     push rbp
     mov rbp, rsp
 
+after_sleep:
     ; open folder
     mov rdi, filename
     mov rsi, O_RDONLY
@@ -49,32 +70,114 @@ _start:
 
     xor rcx, rcx
     
-    file_loop:
-        lea rsi, [dirent + LINUX_DIRENT64.d_name]
-        add rsi, rcx
-        push rcx
-        call printStr
-        mov rsi, newline
-        call printStr
+file_loop:
+    lea rsi, [dirent + rcx + LINUX_DIRENT64.d_name]
+    mov al, byte [rsi]
+    push rcx
+    cmp al, '0'
+    jb loop_continue
+    cmp al, '9'
+    ja loop_continue
+    
+    call check_pid
+    
+
+    loop_continue:
         pop rcx
-        movzx rax, word [dirent + LINUX_DIRENT64.d_reclen]
+        movzx rax, word [dirent + rcx + LINUX_DIRENT64.d_reclen]
         add rcx, rax
         cmp rcx, qword [nread]
-        jae loop_done
+        jae sleep
         jmp file_loop
 
-    loop_done:
+sleep:
+    mov dword [sleeptime + timespec.tv_sec], 10
+    mov dword [sleeptime + timespec.tv_nsec], 0
+    mov rdi, sleeptime
+    mov rsi, sleeptime
+    mov rax, SYS_NANOSLEEP
+    syscall
+
+    jmp after_sleep
 
 
+exit:
     mov rsp, rbp
     pop rbp
 
-exit:
     xor rdi, rdi
     mov rax, SYS_EXIT
     syscall
 
+check_pid:
+    push rbp
+    mov rbp, rsp
+    
+    mov rcx, qword [rsi]
+    mov [pid], rcx
+    mov qword [pidfile], 0
+    mov qword [pidfile + 8], 0
+    mov qword [pidfile + 16], 0
+    mov rbx, pidfile
+    mov rcx, "/proc/"
+    mov [rbx], rcx
+    add rbx, 6
+    xor rcx, rcx
 
+    string_add:
+        mov dl, byte [pid + rcx]
+        cmp dl, 0
+        je add_done
+        mov [rbx], dl 
+        inc rbx
+        inc rcx
+        jmp string_add
+
+    add_done: 
+        mov rcx, "/cmdline"
+        mov [rbx], rcx
+        ; open folder
+        mov rdi, pidfile
+        mov rsi, O_RDONLY
+        xor rdx, rdx
+        mov rax, SYS_OPEN
+        syscall
+
+        cmp rax, -1
+        je check_done
+        mov [pidfd], rax
+
+        mov rdi, qword [pidfd]
+        mov rsi, pidcmd
+        mov rdx, 2
+        mov rax, SYS_READ
+        syscall
+
+        mov rdi, qword [pidfd]
+        mov rax, SYS_CLOSE
+        syscall
+
+        cmp word [pidcmd], "nc"
+        jne check_done
+        mov rsi, pid
+        call printStr
+        mov rsi, newline
+        call printStr
+
+        mov rbx, pid
+        call stringToDecimal
+        mov qword [num], rax
+
+        mov rdi, qword [num]
+        mov rsi, SIGKILL
+        mov rax, SYS_KILL
+        syscall
+
+
+    check_done:
+        mov rsp, rbp
+        pop rbp
+        ret
 
 
 strlen:
@@ -109,3 +212,28 @@ printStr:
     pop rbp
     ret
 
+stringToDecimal:
+    xor rax, rax
+    xor rcx, rcx
+    xor rdx, rdx
+
+    std_continue:
+        mov cl, [rbx]
+        cmp cl, 0x0     ; Check if null character
+        je std_done
+        cmp cl, 0xA     ; Check if enter character
+        je std_done
+
+        sub cl, 0x30 
+        
+        push rcx    ; push ecx value to stack
+        mov rcx, 10 
+        mul rcx
+        pop rcx     ; get value from stack
+        add rax, rcx
+
+        inc rbx
+        jmp std_continue
+
+    std_done:
+        ret 
